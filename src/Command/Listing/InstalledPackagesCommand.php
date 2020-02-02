c76f2911a7a35965e39f0d6f0369bae803c2df6e
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-02-02 16:03:01 -0500

    Temp

--- src/Command/Listing/InstalledPackagesCommand.php
+++ src/Command/Listing/InstalledPackagesCommand.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 namespace ThenLabs\Cli\Command\Listing;
 
-use Symfony\Component\Console\Command\Command;
+use ThenLabs\Cli\Command\ThenCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class InstalledPackagesCommand extends Command
+class InstalledPackagesCommand extends ThenCommand
 {
     protected static $defaultName = 'list:installed-packages';
 
@@ -22,23 +22,8 @@ class InstalledPackagesCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $composerLockFile = $input->getArgument('directory') . '/composer.lock';
-
-        if (! file_exists($composerLockFile)) {
-            $output->writeln('the composer.lock file is missing.');
-            return 0;
-        }
-
-        $content = json_decode(file_get_contents($composerLockFile));
-        if (! is_object($content)) {
-            $output->writeln('the composer.lock file is corrupt.');
-            return 0;
-        }
-
-        foreach ($content->packages as $package) {
-            if ($package->type == 'then-package') {
-                $output->writeln($package->name);
-            }
+        foreach ($this->getInstalledPackages($input, $output) as $package) {
+            $output->writeln($package);
         }
 
         return 0;
---
+++ src/Command/ThenCommand.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace ThenLabs\Cli\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Andy Daniel Navarro Taño <andaniel05@gmail.com>
+ */
+class ThenCommand extends Command
+{
+    public function getInstalledPackages(InputInterface $input, OutputInterface $output): array
+    {
+        $composerLockFile = $input->getArgument('directory') . '/composer.lock';
+        $installedPackages = [];
+
+        if (file_exists($composerLockFile)) {
+            $content = json_decode(file_get_contents($composerLockFile));
+            if (is_object($content)) {
+                foreach ($content->packages as $package) {
+                    if ($package->type == 'then-package') {
+                        $installedPackages[] = $package->name;
+                    }
+                }
+            } else {
+                $output->writeln('the composer.lock file is corrupt.');
+            }
+        } else {
+            $output->writeln('the composer.lock file is missing.');
+        }
+
+        return $installedPackages;
+    }
+}
