c40d9f6cdb42c389016e58053ee12f20fb51d9d3
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2021-03-13 10:26:16 -0500

    Fix

--- src/Command/Assets/ListCommand.php
+++ src/Command/Assets/ListCommand.php
@@ -6,7 +6,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Finder\Finder;
 use Exception;
 
 class ListCommand extends Command
